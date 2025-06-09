f070720d2ca967c9fa69d77208e04cf8c69d41bf
Luan Natan <sousaluan.natan01@gmail.com>
2025-06-09 14:51:35 -0300

    Mudando icones

---
+++ Screens/guia/GuiasScreen.js
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+
+import Assinatura from "./categorias/AssinaturaScreen";
+import Investimento from "./categorias/InvestimentoScreen";
+import Mercado from "./categorias/MercadoScreen";
+import Saude from "./categorias/SaudeScreen";
+import Transporte from "./categorias/TransporteScreen";
+import Vestuario from "./categorias/VestuarioScreen";
+
+const Drawer = createDrawerNavigator();
+
+const icons = {
+  Assinatura: "payment",
+  Investimento: "show-chart",
+  Mercado: "shopping-cart",
+  Saúde: "local-hospital",
+  Transporte: "directions-car",
+  Vestuário: "checkroom",
+};
+
+// Custom Drawer Content para ajustar margin e ícones
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView
+      {...props}
+      contentContainerStyle={{ paddingTop: 50 }} // margem maior para baixo
+    >
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
+export default function GuiasScreen() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={({ route }) => ({
+        headerShown: true,
+        headerTitle: "",
+        headerStyle: {
+          backgroundColor: "#FFB056", // muda cor do header
+        },
+        headerTintColor: "#2A2929", // cor do ícone e texto do header (ex: botão do menu)
+        drawerStyle: {
+          width: 220,
+          backgroundColor: "#FFB056",
+        },
+        drawerLabelStyle: {
+          fontSize: 16,
+          fontWeight: "bold",
+          color: "#333",
+          marginLeft: -16,
+          paddingHorizontal: 10,
+        },
+        drawerActiveTintColor: "#FFA726",
+        drawerInactiveTintColor: "#6e6e6e",
+        drawerActiveBackgroundColor: "#FFECB3",
+        drawerIcon: ({ focused, color, size }) => {
+          const iconName = icons[route.name];
+          return (
+            <MaterialIcons
+              name={iconName}
+              size={size ?? 24}
+              color={focused ? "#FFA726" : "#6e6e6e"}
+            />
+          );
+        },
+      })}
+    >
+      <Drawer.Screen name="Assinatura" component={Assinatura} />
+      <Drawer.Screen name="Investimento" component={Investimento} />
+      <Drawer.Screen name="Mercado" component={Mercado} />
+      <Drawer.Screen name="Saúde" component={Saude} />
+      <Drawer.Screen name="Transporte" component={Transporte} />
+      <Drawer.Screen name="Vestuário" component={Vestuario} />
+    </Drawer.Navigator>
+  );
+}
